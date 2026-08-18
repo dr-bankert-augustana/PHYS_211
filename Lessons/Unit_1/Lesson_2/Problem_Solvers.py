e3f306492cf3d77d957cf94a925905e50f2e8bb5
dr-bankert-augustana <justinbankert@augustana.edu>
2026-08-18 10:11:11 -0500

    Update Problem_Solvers.py

--- Lessons/Unit_1/Lesson_2/Problem_Solvers.py
+++ Lessons/Unit_1/Lesson_2/Problem_Solvers.py
@@ -51,6 +51,48 @@ def solve_1d_average_velocity(values):
     return solution_value, solution_units
 
 ##================================================================================================##
+## Function:    solve_1d_average_acceleration                                                    ##
+##                                                                                                ##
+## Description: Solve 1D average acceleration problems.                                           ##
+##================================================================================================##
+
+def solve_1d_average_acceleration(values):
+
+    ##--------------------------------------------------------------------------------------------##
+    ## Check for Required Variables for this Solver:                                              ##
+    ##--------------------------------------------------------------------------------------------##
+
+    required = ("ti", "tf", "vi", "vf")
+
+    missing = [name for name in required if name not in values]
+
+    if missing: raise ValueError(f"Missing values required for this problem type: {missing}")
+
+    ##--------------------------------------------------------------------------------------------##
+    ## Ensure Divide By Zero is Avoided:                                                          ##
+    ##--------------------------------------------------------------------------------------------##
+
+    delta_t = values["tf"] - values["ti"]
+
+    if abs(delta_t) < 1e-12:
+      
+      raise ValueError("Time interval is zero; cannot calculate average acceleration.")
+
+    ##--------------------------------------------------------------------------------------------##
+    ## Set the Solution:                                                                          ##
+    ##--------------------------------------------------------------------------------------------##
+
+    solution_value = (values["vf"] - values["vi"]) / delta_t
+
+    solution_units = ["m/s^2", "meters per second squared", "mps2", "mpss"]
+
+    ##--------------------------------------------------------------------------------------------##
+    ## Return the Solution Value and Units:                                                       ##
+    ##--------------------------------------------------------------------------------------------##
+
+    return solution_value, solution_units
+
+##================================================================================================##
 ## Function:    solve_1d_motion_with_constant_velocity_v1                                         ##
 ##                                                                                                ##
 ## Description: Solve 1D average velocity problems (for final position).                          ##
@@ -127,6 +169,7 @@ SOLVERS = {
     "1d average velocity": solve_1d_average_velocity,
     "1d motion with constant velocity v1": solve_1d_motion_with_constant_velocity_v1,
     "1d motion with constant velocity v2": solve_1d_motion_with_constant_velocity_v2,
+    "1d average acceleration": solve_1d_average_acceleration,
     #"1d motion with constant acceleration": solve_1d_motion_with_constant_acceleration_v1,
 }
 
@@ -204,6 +247,29 @@ VAR_DICT = {
           "example": 10.7,
         },
     ],
+
+    "1d average acceleration": [
+        {
+          "name": "ti",
+          "description": "initial time (in seconds)",
+          "example": 2.5,
+        },
+        {
+          "name": "tf",
+          "description": "final time (in seconds)",
+          "example": 20.8,
+        },
+        {
+          "name": "vi",
+          "description": "initial velocity (in meters per second)",
+          "example": 5.3,
+        },
+        {
+          "name": "vf",
+          "description": "final velocity (in meters per second)",
+          "example": 63.7,
+        },
+    ],
 }
 
 ##================================================================================================##
@@ -227,6 +293,12 @@ TAR_DICT = {
     "1d motion with constant velocity v2" : {
         "name": "tf",
         "description": "final time (in seconds)",
-        "example": 22.5,
+        "example": 22.5,:
+      },
+
+    "1d average acceleration" : {
+        "name": "a",
+        "description": "average acceleration (in meters per second squared)",
+        "example": 4.5,
       },
 }
